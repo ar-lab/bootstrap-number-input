d77a9fc1c12c1efdb8aff43b00edc0c3b6a61dea
alex-ray <alex-ray@land.ru>
2017-03-01 17:59:21 +0300

    Compatible for Bootstrap 2.3.2

--- bootstrap-number-input.js
+++ bootstrap-number-input.js
@@ -26,18 +26,21 @@
         var settings = $.extend({
             upClass: 'default',
             downClass: 'default',
+            upText: '+',
+            downText: '-',
             center: true
         }, options);
 
-        return this.each(function (e) {
+        return this.each(function () {
             var self = $(this);
             var clone = self.clone();
 
             var min = self.attr('min');
             var max = self.attr('max');
+            var step = parseInt(self.attr('step')) || 1;
 
             function setText(n) {
-                if ((min && n < min) || (max && n > max)) {
+                if (isNaN(n) || (min && n < min) || (max && n > max)) {
                     return false;
                 }
 
@@ -45,37 +48,30 @@
                 return true;
             }
 
-            var group = $("<div class='input-group'></div>");
-            var down = $("<button type='button'>-</button>").attr('class', 'btn btn-' + settings.downClass).click(function () {
-                setText(parseInt(clone.val()) - 1);
+            var group = $("<div class='input-group input-prepend input-append'></div>");
+            var down = $("<button type='button'>" + settings.downText + "</button>").attr('class', 'btn btn-' + settings.downClass).click(function() {
+                setText(parseInt(clone.val() || clone.attr('value')) - step);
             });
-            var up = $("<button type='button'>+</button>").attr('class', 'btn btn-' + settings.upClass).click(function () {
-                setText(parseInt(clone.val()) + 1);
+            var up = $("<button type='button'>" + settings.upText + "</button>").attr('class', 'btn btn-' + settings.upClass).click(function() {
+                setText(parseInt(clone.val() || clone.attr('value')) + step);
             });
-            $("<span class='input-group-btn'></span>").append(down).appendTo(group);
+            $(down).appendTo(group);
             clone.appendTo(group);
-            if (clone) {
+            if(clone && settings.center) {
                 clone.css('text-align', 'center');
             }
-            $("<span class='input-group-btn'></span>").append(up).appendTo(group);
+            $(up).appendTo(group);
 
             // remove spins from original
             clone.prop('type', 'text').keydown(function (e) {
                 if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-					(e.keyCode == 65 && e.ctrlKey === true) ||
-					(e.keyCode >= 35 && e.keyCode <= 39)) {
+                    (e.keyCode == 65 && e.ctrlKey === true) ||
+                    (e.keyCode >= 35 && e.keyCode <= 39)) {
                     return;
                 }
                 if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
                     e.preventDefault();
                 }
-
-                var c = String.fromCharCode(e.which);
-                var n = parseInt(clone.val() + c);
-
-                //if ((min && n < min) || (max && n > max)) {
-                //    e.preventDefault();
-                //}
             });
 
             clone.prop('type', 'text').blur(function (e) {
@@ -88,7 +84,6 @@
                     setText(max);
                 }
             });
-
 
             self.replaceWith(group);
         });
